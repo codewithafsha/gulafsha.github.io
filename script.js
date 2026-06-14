f4f7d7ef4861c7dbd74e55fe93db9371d459bc9c
codewithafsha <gk78697703@gmail.com>
2026-06-14 18:41:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("welcome to Afsha's website");
+console.log("Tjavascript is working");
+document.getElementById("message").innerHTML = "Hello Afsha";
+document.body.style.backgroundColor = "lightblue";
+let name = "Afsha Khan";
+alert("My name is " + name);
+let age = 20;
+console.log(age);
